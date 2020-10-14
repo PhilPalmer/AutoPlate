032a26e47de7f729b8a43dff3144c111692b67e8
PhilPalmer <phil.palmer.email@gmail.com>
2020-10-14 16:59:17 +0100

    Changed tab name from analyse -> results

--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@ dashboardPage(
             menuItem("Home", tabName = "home", icon = icon("home")),
             menuItem("1) Input", tabName = "input", icon = icon("sign-in-alt")),
             menuItem("2) QC", tabName = "qc", icon = icon("check-square")),
-            menuItem("3) Analyse", tabName = "analyse", icon = icon("chart-line"))
+            menuItem("3) Results", tabName = "results", icon = icon("chart-line"))
         )
     ),
     dashboardBody(
@@ -56,10 +56,10 @@ dashboardPage(
                         )
                     )
             ),
-            tabItem(tabName = "analyse",
+            tabItem(tabName = "results",
                     fluidRow(
                         box(
-                            title = "3) Analyse"
+                            title = "3) Results"
                         )
                     )
             )
