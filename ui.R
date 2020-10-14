bfbbf440bd98e29ece70ee9dc2564e6565904eb8
PhilPalmer <phil.palmer.email@gmail.com>
2020-10-14 10:47:53 +0100

    Added sidebar menu items

--- ui.R
+++ ui.R
@@ -2,6 +2,13 @@ library(shinydashboard)
 
 dashboardPage(
     dashboardHeader(),
-    dashboardSidebar(),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Home", tabName = "home", icon = icon("home")),
+            menuItem("1) Input", tabName = "input", icon = icon("sign-in-alt")),
+            menuItem("2) QC", tabName = "qc", icon = icon("check-square")),
+            menuItem("3) Analyse", tabName = "analyse", icon = icon("chart-line"))
+        )
+    ),
     dashboardBody()
 )
