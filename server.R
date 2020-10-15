9e954dbf96336f7053cd8aee8681d5a52759323c
PhilPalmer <phil.palmer.email@gmail.com>
2020-10-15 17:16:10 +0100

    Corrected plate size to 96 wells/cells

--- server.R
+++ server.R
@@ -15,9 +15,9 @@ function(input, output, sessions) {
     dilution_values <- reactiveValues()
     metadata_values <- reactiveValues()
     # Create empty df for metadata
-    groups <- paste("Group",LETTERS[seq( from = 1, to = 12)])
+    groups <- paste("Group",LETTERS[seq( from = 1, to = 6)])
     groups <- sort(c(paste0(groups,":1"),paste0(groups,":2")))
-    metadata <- data.frame(matrix(0, nrow = 8, ncol = 24))
+    metadata <- data.frame(matrix(0, nrow = 8, ncol = 12))
     names(metadata) <- groups
     rownames(metadata) <- c(paste0("V",1:5),paste0("C",1:3))
     
