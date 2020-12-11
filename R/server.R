3d4e923a74df6faafe2e11aaed4888a8b0ec1360
PhilPalmer <phil.palmer.email@gmail.com>
2020-12-11 10:34:02 +0000

    Increased the size of svg plot downloads

--- R/server.R
+++ R/server.R
@@ -402,19 +402,19 @@ server <- function(input, output, session) {
   # Download plots
   output$download_data_exploration <- downloadHandler(
     filename = "data_exploration.svg",
-    content = function(file) ggsave(file, plot = values[["data_exploration"]])
+    content = function(file) ggsave(file, plot = values[["data_exploration"]], width = 10)
   )
   output$download_drc <- downloadHandler(
     filename = "drc.svg",
-    content = function(file) ggsave(file, plot = values[["drc"]])
+    content = function(file) ggsave(file, plot = values[["drc"]], width = 10)
   )
   output$download_ic50 <- downloadHandler(
     filename = "ic50_boxplot.svg",
-    content = function(file) ggsave(file, plot = values[["ic50_boxplot"]])
+    content = function(file) ggsave(file, plot = values[["ic50_boxplot"]], width = 10)
   )
   output$download_cv_boxplot <- downloadHandler(
     filename = "cv_boxplot.svg",
-    content = function(file) ggsave(file, plot = values[["cv_boxplot"]])
+    content = function(file) ggsave(file, plot = values[["cv_boxplot"]], width = 10)
   )
 
   # Generate and render results plots
