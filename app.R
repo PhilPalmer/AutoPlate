0257f4fdafe6788bb542142c0b77a6dcdf55fb8b
PhilPalmer <phil.palmer.email@gmail.com>
2021-01-20 16:48:38 +0000

    Added library dependencies back

--- app.R
+++ app.R
@@ -4,6 +4,11 @@
 # To deploy, run: rsconnect::deployApp(appTitle="AutoPlate")
 # Or use the blue button on top of this file
 
+# Load dependencies
+packages <- c("shiny","shinydashboard","rhandsontable","tidyr","dplyr","plot.matrix","viridis","ggplot2","drc","rmarkdown","knitr","svglite","plotly","shinyWidgets","metafolio")
+invisible(lapply(packages, library, character.only = TRUE))
+
+# Run app
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
 autoplate::run_app() # add parameters here (if any)
