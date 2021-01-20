983c2b83a217c76c474f0c626e4d87d5d2449996
PhilPalmer <phil.palmer.email@gmail.com>
2021-01-20 16:49:34 +0000

    Fixed link to moved svg file

--- R/app_ui.R
+++ R/app_ui.R
@@ -37,7 +37,7 @@ app_ui <- function(request) {
                     shinydashboard::box(
                       width = 12, status = "primary",
                       shiny::div(shiny::img(
-                        src = "images/virus.svg",
+                        src = "inst/app/www/images/virus.svg",
                         width = 300, id = "logo"
                       ),
                       style = "text-align:left; float:right;"
